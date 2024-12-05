0ea5e80e2364f121a5608470ae85cb3457584d4e
Adarshika Pandey <153723733+adarshika2005@users.noreply.github.com>
2024-12-05 19:37:27 +0530

    Add files via upload

---
+++ s2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter the value of a is "<<endl;
+    cin>>a;
+    cout<<"enter the value of b is "<<endl;
+    cin>>b;
+    char op;
+    cout<<"enter the operation u want to perform"<<endl;
+    cin>>op;
+    switch(op){
+        case '+':cout<<(a+b)<<endl;
+        break;
+        case '-':cout<<(a-b)<<endl;
+        break;
+        case '*':cout<<(a*b)<<endl;
+        break;
+        case '/':cout<<(a/b)<<endl;
+        break;
+        case '%':cout<<(a%b)<<endl;
+        break;
+        default:cout<<"please enter valid operation"<<endl;
+        
+    }
+}
